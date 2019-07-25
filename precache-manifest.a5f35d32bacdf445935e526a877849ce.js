1f1ef09a5197b4088d6dbdd49c8230d561873185
Asjad Khalil <asjadkhalil@gmail.com>
2019-07-25 05:02:33 +0500

    Updates

---
+++ precache-manifest.a5f35d32bacdf445935e526a877849ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe51d4e9f64f097563ea1925744646b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "b278fbee7140ccebecac",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5f1ec59d7e5e340aecd8",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b278fbee7140ccebecac",
+    "url": "/contactmanager/static/js/2.9474b5f3.chunk.js"
+  },
+  {
+    "revision": "5f1ec59d7e5e340aecd8",
+    "url": "/contactmanager/static/js/main.b3a2a724.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
